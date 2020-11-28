4a10357fcad0dd20577bd9a91df87a41078e6d4d
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-11-29 01:26:06 +0530

    Add vectors

---
+++ Vectors/Vectors/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+struct Vertex {
+	float x, y, z;
+};
+
+std::ostream& operator<<(std::ostream& stream, const Vertex& vertex) {
+	stream << vertex.x << ", " << vertex.y << ", " << vertex.z;
+	return stream;
+}
+
+int main() {
+	std::vector<Vertex> vertices;
+	vertices.push_back({ 1, 2, 3 });
+	vertices.push_back({ 4, 5, 6 });
+
+	for (int i = 0; i < vertices.size(); i++) {
+		std::cout << vertices[i] << std::endl;
+	}
+
+	//vertices.clear(); // Will clear out vector
+	vertices.erase(vertices.begin() + 1); // remove the second element 
+
+	for (Vertex& v : vertices) {
+		std::cout << v << std::endl;
+	}
+
+	std::cin.get();
+}
+
+// Pass vectors by reference
